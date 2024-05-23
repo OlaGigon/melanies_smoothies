845808037fb2221dd183ea85e54559d43f7dd644
Ola Gigon <44475277+OlaGigon@users.noreply.github.com>
2024-05-23 22:16:33 +0200

    Update streamlit_app.py
    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     , my_dataframe
-    , max_selections=6)
+    , max_selections=5)
 
 if ingredients_list:
 
@@ -42,3 +42,7 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     
     st.success('Your Smoothie is ordered!'+ ' ' +name_on_order  , icon="✅")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
